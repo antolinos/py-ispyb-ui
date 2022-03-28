da4948b6442ee0746abee0ae80830849ec65f7c8
Alejandro De Maria Antolinos <demariaa@esrf.fr>
2022-03-28 13:56:04 +0200

    updated config

--- src/config/sites.ts
+++ src/config/sites.ts
@@ -1,43 +1,42 @@
-import { Site } from 'models';
+import { Site } from "models";
 
 const sites: Site[] = [
   {
-    name: 'ESRF',
-    //server: 'http://lgaonach:5000/ispyb/api/v1/legacy',
-    server: 'https://ispyb.esrf.fr/ispyb/ispyb-ws/rest',
-    description: 'European Synchroton Radiation Facility',
-    icon: '../images/site/esrf.png',
+    name: "ESRF",
+    server: "https://ispyb.esrf.fr/ispyb/ispyb-ws/rest",
+    description: "European Synchroton Radiation Facility",
+    icon: "../images/site/esrf.png",
     authentication: {
       sso: {
         enabled: false,
-        plugin: 'keycloak',
+        plugin: "keycloak",
         configuration: {
-          realm: 'ESRF',
-          url: 'https://websso.esrf.fr/auth/',
-          clientId: 'icat',
+          realm: "ESRF",
+          url: "https://websso.esrf.fr/auth/",
+          clientId: "icat",
         },
       },
       authenticators: [
         {
-          plugin: 'db',
-          title: 'ISPyB',
-          server: 'https://ispyb.esrf.fr/ispyb/ispyb-ws/rest',
+          plugin: "db",
+          title: "ISPyB",
+          server: "https://ispyb.esrf.fr/ispyb/ispyb-ws/rest",
           enabled: true,
-          site: 'ESRF',
-          message: 'Use ISPyB authentication when you log in as a proposal',
+          site: "ESRF",
+          message: "Use ISPyB authentication when you log in as a proposal",
         },
       ],
     },
 
     techniques: {
-      SAXS: { beamlines: [{ name: 'BM29' }] },
-      EM: { beamlines: [{ name: 'CM01' }] },
+      SAXS: { beamlines: [{ name: "BM29" }] },
+      EM: { beamlines: [{ name: "CM01" }] },
       MX: {
         beamlines: [
           { name: 'ID23-1', sampleChangerType: 'FlexHCDDual' },
           { name: 'ID23-2', sampleChangerType: 'FlexHCDUnipuckPlate' },
           { name: 'ID29', sampleChangerType: 'FlexHCDDual' },
-          { name: 'ID30A-1', sampleChangerType: 'RoboDiffHCDSC3' },
+          { name: 'ID30A-1', sampleChangerType: 'FlexHCDUnipuckPlate' },
           { name: 'ID30A-2', sampleChangerType: 'FlexHCDDual' },
           { name: 'ID30A-3', sampleChangerType: 'FlexHCDDual' },
           { name: 'ID30B', sampleChangerType: 'FlexHCDDual' },
@@ -47,72 +46,107 @@ const sites: Site[] = [
     },
   },
   {
-    name: 'ESRF EM',
-    server: 'https://ispyb.esrf.fr/ispyb/ispyb-ws/rest',
-    description: 'European Synchroton Radiation Facility',
-    icon: '../images/site/esrf.png',
+    name: "ESRF EM(only)",
+    server: "https://ispyb.esrf.fr/ispyb/ispyb-ws/rest",
+    description: "European Synchroton Radiation Facility",
+    icon: "../images/site/esrf.png",
     authentication: {
       sso: {
-        enabled: process.env.REACT_APP_SSO_AUTH_ENABLED === 'true',
-        plugin: 'keycloak',
+        enabled: process.env.REACT_APP_SSO_AUTH_ENABLED === "true",
+        plugin: "keycloak",
         configuration: {
-          realm: 'ESRF',
-          url: 'https://websso.esrf.fr/auth/',
-          clientId: 'icat',
+          realm: "ESRF",
+          url: "https://websso.esrf.fr/auth/",
+          clientId: "icat",
         },
       },
       authenticators: [
         {
-          plugin: 'db',
-          title: 'ISPyB',
-          server: 'https://ispyb.esrf.fr/ispyb/ispyb-ws/rest',
+          plugin: "db",
+          title: "ISPyB",
+          server: "https://ispyb.esrf.fr/ispyb/ispyb-ws/rest",
           enabled: true,
-          site: 'ESRF',
-          message: 'Use ISPyB authentication when you log in as a proposal',
+          site: "ESRF",
+          message: "Use ISPyB authentication when you log in as a proposal",
         },
       ],
     },
 
     techniques: {
-      EM: { beamlines: [{ name: 'CM01' }] },
+      EM: { beamlines: [{ name: "CM01" }] },
     },
   },
+  
+
   {
-    name: 'Mael SSO',
-    server: 'http://lgaonach:5000/ispyb/api/v1/legacy',
-    description: 'European Synchroton Radiation Facility',
-    icon: '../images/site/esrf.png',
+    name: "EMBL",
+    server: "https://ispyb.embl-hamburg.de/ispyb/ispyb-ws/rest",
+    description: "European Molecular Biology Laboratory",
+    icon: "../images/site/esrf.png",
     authentication: {
       sso: {
-        enabled: process.env.REACT_APP_SSO_AUTH_ENABLED === 'true',
-        plugin: 'keycloak',
-        configuration: {
-          realm: 'ESRF',
-          url: 'https://websso.esrf.fr/auth/',
-          clientId: 'icat',
-        },
+        enabled: false,
       },
       authenticators: [
         {
-          plugin: 'db',
-          title: 'ISPyB',
-          server: 'https://ispyb.esrf.fr/ispyb/ispyb-ws/rest',
+          plugin: "db",
+          title: "ISPyB",
+          server: "https://ispyb.embl-hamburg.de/ispyb/ispyb-ws/rest",
           enabled: true,
-          site: 'ESRF',
-          message: 'Use ISPyB authentication when you log in as a proposal',
+          site: "EMBL",
+          message: "Use ISPyB authentication when you log in as a proposal",
         },
       ],
     },
 
     techniques: {
-      SAXS: { beamlines: [{ name: 'BM29' }] },
-      EM: { beamlines: [{ name: 'CM01' }] },
       MX: {
         beamlines: [
+          {
+            name: "P13",
+            
+          },
+          {
+            name: "P14",
+            
+          }
+        ],
+      },
+    },
+  },
+
+  {
+    name: "MAXIV",
+    server: "https://ispyb.maxiv.lu.se/ispyb/ispyb-ws/rest",
+    description: "MAX IV Laboratory",
+    icon: "../images/site/esrf.png",
+    authentication: {
+      sso: {
+        enabled: false,
+      },
+      authenticators: [
+        {
+          plugin: "db",
+          title: "ISPyB",
+          server: "https://ispyb.maxiv.lu.se/ispyb/ispyb-ws/rest",
+          enabled: true,
+          site: "MAXIV",
+          message: "Use ISPyB authentication when you log in as a proposal",
+        },
+      ],
+    },
+
+    techniques: {
+      EM: {
+        beamlines: [
+          {
+            name: "BioMAX",
+            //sampleChangerType: "ISARA",
+          },
           { name: 'ID23-1', sampleChangerType: 'FlexHCDDual' },
           { name: 'ID23-2', sampleChangerType: 'FlexHCDUnipuckPlate' },
           { name: 'ID29', sampleChangerType: 'FlexHCDDual' },
-          { name: 'ID30A-1', sampleChangerType: 'RoboDiffHCDSC3' },
+          { name: 'ID30A-1', sampleChangerType: 'FlexHCDUnipuckPlate' },
           { name: 'ID30A-2', sampleChangerType: 'FlexHCDDual' },
           { name: 'ID30A-3', sampleChangerType: 'FlexHCDDual' },
           { name: 'ID30B', sampleChangerType: 'FlexHCDDual' },
@@ -124,3 +158,4 @@ const sites: Site[] = [
 ];
 
 export default sites;
+
